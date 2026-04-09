e0dd7ccfbd69304ca327f7b2a4472083424b43d2
Constantin Maes <constantinmaes@gmail.com>
2026-04-09 17:52:53 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,67 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const express = require('express');
+const app = express();
+
+const axios = require('axios');
+
+// Fonction pour obtenir le token OAuth2
+async function generateAccessToken() {
+    const auth = Buffer.from(
+        `${process.env.PAYPAL_CLIENT_ID}:${process.env.PAYPAL_CLIENT_SECRET}`,
+    ).toString('base64');
+    const { data } = await axios.post(
+        `${process.env.PAYPAL_BASE_URL}/v1/oauth2/token`,
+        {
+            grant_type: 'client_credentials',
+        },
+        {
+            headers: {
+                Authorization: `Basic ${auth}`,
+                'content-type': 'application/x-www-form-urlencoded',
+            },
+        },
+    );
+    return data.access_token;
+}
+
+// Routes
+app.get('/api/orders', async (req, res) => {
+    const token = await generateAccessToken();
+    console.log('Access Token:', token);
+    const { data, status } = await axios.post(
+        process.env.PAYPAL_BASE_URL + '/v2/checkout/orders',
+        {
+            intent: 'CAPTURE',
+            purchase_units: [
+                {
+                    amount: {
+                        currency_code: 'EUR',
+                        value: '100.00',
+                    },
+                },
+            ],
+            /* NEW */
+            payment_source: {
+                paypal: {
+                    experience_context: {
+                        return_url: 'http://localhost:3000/success',
+                        cancel_url: 'http://localhost:3000/cancel',
+                        user_action: 'PAY_NOW',
+                    },
+                },
+            },
+        },
+        {
+            headers: {
+                Authorization: 'Bearer ' + token,
+            },
+        },
+    );
+    res.status(status).json(data);
+});
+
+app.listen(3000, () => {
+    console.log('Server listening on port 3000');
+});
